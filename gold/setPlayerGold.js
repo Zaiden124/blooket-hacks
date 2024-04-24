ead82aca697824cde2942a515dc6767d4fffb6b6
Zaiden124 <168021089+Zaiden124@users.noreply.github.com>
2024-04-24 13:23:51 -0500

    Update setPlayerGold.js

--- gold/setPlayerGold.js
+++ gold/setPlayerGold.js
@@ -14,8 +14,8 @@
         alert('You must be in a gold quest game!');
     } else {
         let e = reactHandler(),
-            player = prompt("Player name to set gold"),
-            amount = Number(parseFloat(prompt("Amount to set gold to")));
+            player = prompt("Zaiden"),
+            amount = Number(parseFloat(prompt("10000000000000")));
         e.memoizedProps.firebase.setVal({
             id: e.memoizedProps.client.hostId,
             path: "c/" + e.memoizedProps.client.name,
